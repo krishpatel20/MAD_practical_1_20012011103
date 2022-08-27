546d75911d3b79f79ee013e58cfdf2f08b7d2c0d
krishpatel2383 <krishpatel2383@gmail.com>
2022-08-27 14:28:43 +0530

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main()
+{
+// Defining Variables :
+    val i: Int = 22
+    val f: Float = 1.5F
+    val c: Char = 'T'
+    val st: String = "HMS"
+    val bl: Boolean = false
+    val d: Double = 96.36
+    val l: Long = 338927847
+    val s: Short = -2
+    val b: Byte = 127
+// Printing all the Variables :
+    println("Integer Value: $i")
+    println("Float Value: $f")
+    println("Character Value: $c")
+    println("String Value: $st")
+    println("Boolean Value: $bl")
+    println("Double Value: $d")
+    println("Long Value: $l")
+    println("Short Value: $s")
+    println("Byte Value: $b")
+}
---
+++ src/main/kotlin/pr1_2.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val a:Int = 10
+    val b = a.toDouble()
+    val g: String = "10"
+    val f: String = "11.12"
+    val c = Integer.parseInt(g)
+    val d = g.toInt()
+    val e = f.toDouble()
+    println("20012011103 Krish Patel")
+    println("Integer Value:$a")
+    println("Double Value (From Integer):$b")
+    println("String Value:$g")
+    println("Integer Value1 (From String):$c")
+    println("Integer Value2 (From String):$d")
+    println("Double Value (From String):$e")
+}
